239b6d953c83e37aadad5c40b0c382c713f82977
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2021-03-02 18:30:33 +0100

    Add sequence example

---
+++ src/test/kotlin/SequenceTest.kt
@@ -0,0 +1,43 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SequenceTest {
+
+    /*
+    Sequences do not creates temporary collections. It may be more efficient when processing large collections
+    */
+    @Test
+    fun `Elements should be processed sequentially`(){
+
+        val people = listOf(
+                Person(name = "John", age = 15),
+                Person(name = "Mike", age = 43),
+                Person(name = "Brian", age = 84),
+                Person(name = "Alice", age = 68),
+                Person(name = "Bob", age = 53) )
+
+        val trace = StringBuilder()
+        val result = people.asSequence()
+                .map { trace.append("${it.name} -> map\n"); it.name }
+                .filter { trace.append("$it -> filter\n"); it.startsWith("B") }
+                .toList()
+
+        println(result)
+
+        val expectedResult = """
+        |John -> map
+        |John -> filter
+        |Mike -> map
+        |Mike -> filter
+        |Brian -> map
+        |Brian -> filter
+        |Alice -> map
+        |Alice -> filter
+        |Bob -> map
+        |Bob -> filter
+        """.trimMargin()+"\n"
+
+        val actualResult = trace.toString()
+        assertEquals(expectedResult, actualResult)
+    }
+}
